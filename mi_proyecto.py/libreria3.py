0acf613650c6340358656c9116612f3cc5713b45
Martin69696969696969696969 <24040015@utculiacan.edu.mx>
2025-08-26 08:17:18 -0700

    ejercicio 18

---
+++ mi_proyecto.py/libreria3.py
@@ -0,0 +1,7 @@
+def ascii_art():
+    arte = """
+     (•_•)
+    <)   )╯  Hola desde libreria3
+     /   \\
+    """
+    return arte
---
+++ mi_proyecto.py/main.py
@@ -0,0 +1,11 @@
+import libreria1
+import libreria2
+import libreria3
+
+print(libreria1.saludo("Martin"))
+
+resultado = libreria2.suma(5, 7)
+print("Resultado de la suma:", resultado)
+print("El valor de PI es:", libreria2.PI)
+
+print(libreria3.ascii_art())
